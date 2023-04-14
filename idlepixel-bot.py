311ae1fc467bc1011ae462c1b4a138effb4cfcd4
lux-ferre <milne_christopher@hotmail.com>
2023-04-14 21:55:25 +0100

    - Bug fix for Bug fix for websocket send handler

--- idlepixel-bot.py
+++ idlepixel-bot.py
@@ -38,7 +38,7 @@ testing_webhook = SyncWebhook.from_url(env_consts["TESTING_HOOK_URL"])
 
 def on_web_socket(ws):
     print(f"WebSocket opened: {ws.url}")
-    ws.on("framesent", on_message_send())
+    ws.on("framesent", on_message_send)
     ws.on("framereceived", receive_message)
     ws.on("close", handle_disconnect)
 
