5115e531299f5834b266c4c9b42ccb1c72307a26
lux-ferre <milne_christopher@hotmail.com>
2023-04-14 21:15:37 +0100

    - Handle custom messages - ModMod bot features started

--- idlepixel-bot.py
+++ idlepixel-bot.py
@@ -27,6 +27,7 @@ for key in env_consts:
 
 idle_pixel_connected = False
 development_mode = True
+online_mods = []
 
 global page
 
@@ -42,7 +43,7 @@ def on_web_socket(ws):
     ws.on("close", handle_disconnect)
 
 
-def receive_message(raw_message: str):
+async def receive_message(raw_message: str):
     split_message = raw_message.split("=", 1)
     if len(split_message) > 1:
         [message_type, message_data] = split_message
@@ -57,6 +58,8 @@ def receive_message(raw_message: str):
         on_chat(message_data)
     elif message_type == "YELL":
         on_yell(message_data)
+    elif message_type == "CUSTOM":
+        await on_custom(message_data)
     else:
         print(f"{message_type}: {message_data}")
 
@@ -86,6 +89,55 @@ def on_yell(data: str):
     formatted_chat = f'*[{current_time}]* **SERVER MESSAGE:** {data} '
 
     log_message(formatted_chat)
+
+
+async def on_custom(data: str):
+    [player, data_packet] = data.split("~")
+    split_packet = data_packet.split(":", 3)
+    callback_id = split_packet[0]
+    plugin = split_packet[1]
+    command = split_packet[2]
+    content = split_packet[3]
+
+    print(f"'{command}' received message with id '{callback_id}' and content '{content}' from {player}.")
+
+    if plugin == "interactor":
+        await handle_interactor(player, command, content)
+    elif plugin == "MODMOD":
+        await handle_modmod(player, command, content)
+
+
+async def handle_interactor(player: str, command: str, content: str):
+    if command == "echo":
+        await send_custom_message(player, content)
+    elif command == "chatecho":
+        await send_chat_message(player, content)
+
+
+async def handle_modmod(player: str, command: str, content: str):
+    if command == "HELLO":
+        if content == "1:0":
+            online_mods.append(player)
+            await send_mod_message(f"MODMOD:MSG:{player} has logged in!")
+        elif content == "0:0":
+            await send_custom_message(player, "0:0")
+    elif command == "MODCHAT":
+        await send_mod_message(f"MODMOD:MSG:{player}: {content}")
+
+
+async def send_mod_message(content: str):
+    for account in online_mods:
+        await send_custom_message(account, content)
+
+
+async def send_custom_message(player: str, content: str):
+    custom_string = f"CUSTOM={player}~{content}"
+    await page.evaluate(f"window.websocket.send('{custom_string}')")
+
+
+async def send_chat_message(player: str, content: str):
+    chat_string = f"CHAT={player} echo: {content}"
+    await page.evaluate(f"window.websocket.send('{chat_string}')")
 
 
 def log_message(message: str):
